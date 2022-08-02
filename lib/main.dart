133f37053cc1085ce51f4698a1c97b4e8ad6fd3a
Jonas Emir <jonasemir00@gmail.com>
2022-08-01 22:30:05 -0300

    EtÃapa de transferência

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children: const <Widget>[
-          Text(
-            'Deliver features faster',
-            textDirection: TextDirection.ltr,
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    body: ListaTransferencias(),
+    appBar: AppBar(title: Text('Transferências'),),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {  },
+      child: Icon(Icons.add),
+    ),
+  ),
+));
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('100.0'),
+            subtitle: Text('1000'),
           ),
-          Text(
-            'Craft beautiful UIs',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Craft beautiful UIs',
-            textDirection: TextDirection.ltr,
-          ),
-          Column(
-            children: <Widget>[
-              Text(
-                'Craft beautiful UIs',
-                textDirection: TextDirection.ltr,
-              ),
-            ],
-          )
-        ],
-      ),
+        ),
+        Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('200.0'),
+              subtitle: Text('1000'),
+            )
+        ),
+        Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('300.0'),
+              subtitle: Text('1000'),
+            )
+        ),
+      ],
     );
+  }
+}
